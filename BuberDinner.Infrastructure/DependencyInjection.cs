262f8705770dbb4de7911fbde502b31a52f10726
Sankalp Poinwar <SankalpP@centiro.se>
2023-04-27 19:00:37 +0530

    Adds project developed till know;

---
+++ BuberDinner.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Infrastructure.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using BuberDinner.Application.Common.Interfaces.Persistence;
+using BuberDinner.Infrastructure.Persistence;
+
+namespace BuberDinner.Infrastructure;
+
+public static class DependencyInjection{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        ConfigurationManager configuration){
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        return services;
+    }
+}
