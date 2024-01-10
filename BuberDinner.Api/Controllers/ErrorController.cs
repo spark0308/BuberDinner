6c4090ce2621b5cad7fdf8a7db8ee6c9606a2f14
Sankalp Poinwar <SankalpP@centiro.se>
2024-01-10 21:55:35 +0530

    Implements error handler and exception flow; Learns about ProblemDetailsFactory and its customized method; Learns about ErrorOr;

--- BuberDinner.Api/Controllers/ErrorController.cs
+++ BuberDinner.Api/Controllers/ErrorController.cs
@@ -1,3 +1,4 @@
+using BuberDinner.Application.Common.Errors;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,6 +10,12 @@ public class ErrorController : ControllerBase
     public IActionResult Error()
     {
         Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
-        return Problem(title: exception?.Message);
+
+        var (statusCode, message) = exception switch {
+            IServiceException serviceException => ((int)serviceException.StatusCode, serviceException.ErrorMessage),
+            _ => (StatusCodes.Status500InternalServerError, "An unexpected error occured.")
+        };
+
+        return Problem(statusCode:statusCode, title: message);
     }
 }
